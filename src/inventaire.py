978174bf73f088261746acc02e6fd6d4ee93ac7e
Antho3004 <anthony.iv14@gmail.com>
2023-11-20 18:31:33 +0100

    système de filtre V1

--- src/inventaire.py
+++ src/inventaire.py
@@ -10,14 +10,29 @@ class Inventaire(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def show_inventory(self, ctx: commands.Context, user, page):
-        cursor.execute("SELECT COUNT(*) FROM user_inventaire WHERE user_id = ?", (str(user.id),))
-        count = cursor.fetchone()[0]
+    async def show_inventory(self, ctx, user, name):
+
+        if name:
+            cursor.execute(f"SELECT COUNT(*) FROM user_inventaire WHERE user_id = ? AND nom = ?", (str(user.id), name))
+            count = cursor.fetchone()[0]
+        else:
+            cursor.execute(f"SELECT COUNT(*) FROM user_inventaire WHERE user_id = ?", (str(user.id),))
+            count = cursor.fetchone()[0]
 
         if count > 0:
-            cursor.execute("SELECT code_card, nom, groupe, rarete FROM user_inventaire WHERE user_id = ? ORDER BY groupe, nom, rarete, CAST(SUBSTR(code_card, INSTR(code_card, '-') + 1) AS INTEGER)", (str(user.id),))
+            if name:
+                cursor.execute(f"SELECT code_card, nom, groupe, rarete FROM user_inventaire WHERE user_id = ? AND nom = ? ORDER BY groupe, nom, rarete, CAST(SUBSTR(code_card, INSTR(code_card, '-') + 1) AS INTEGER)", (str(user.id), name))
+                print(cursor)
+            else:
+                cursor.execute(f"SELECT code_card, nom, groupe, rarete FROM user_inventaire WHERE user_id = ? ORDER BY groupe, nom, rarete, CAST(SUBSTR(code_card, INSTR(code_card, '-') + 1) AS INTEGER)", (str(user.id),))
 
             result = cursor.fetchall()
+
+            if not result:
+            # Aucun résultat trouvé avec le nom spécifié
+                embed = discord.Embed(title=f"{user.name}'s inventory", description=f"No cards found in your inventory.", color=discord.Color.red())
+                await ctx.send(embed=embed)
+                return
 
             rarity_emojis = {
                 "C": "<:C_:1107771999490686987>",
@@ -27,7 +42,6 @@ class Inventaire(commands.Cog):
                 "L": "<:L_:1107772002690945055>"
             }
 
-            # Divisez vos données en morceaux de 9 cartes par page
             chunks = [result[i:i + 9] for i in range(0, len(result), 9)]
 
             embeds = []
@@ -39,14 +53,10 @@ class Inventaire(commands.Cog):
                     embed.add_field(
                         name=f"{line[0]}", value=f"Name: {line[1]}\nGroup: {line[2]}\nRarity: {rarity_emojis.get(line[3], '')}\n", inline=True)
 
-                # Ajoutez le nombre de cartes au pied de l'embed
                 embed.set_footer(text=f"Total cards: {count}")
                 embeds.append(embed)
 
-            # Créez un objet Paginator en passant la liste d'embeds
             paginator = Paginator(embeds)
-
-            # Démarrez la pagination
             await paginator.start(ctx)
 
         else:
@@ -54,18 +64,18 @@ class Inventaire(commands.Cog):
             await ctx.send(embed=embed)
 
     @commands.command()
-    async def inventory(self, ctx, user: discord.Member = None, page: int = 1):
+    async def inventory(self, ctx, user: discord.Member = None, name=None):
         if user is None:
             user = ctx.author
 
-        await self.show_inventory(ctx, user, page)
+        await self.show_inventory(ctx, user, name)
 
     @commands.command(name='inv')
-    async def shortcut_inventory(self, ctx, user: discord.Member = None, page: int = 1):
+    async def shortcut_inventory(self, ctx, user: discord.Member = None, name=None):
         if user is None:
             user = ctx.author
 
-        await self.show_inventory(ctx, user, page)
+        await self.show_inventory(ctx, user, name)
 
 async def setup(bot):
     await bot.add_cog(Inventaire(bot))
