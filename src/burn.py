6afc8ea1494d052e39bdb3b65c734b9294f427ed
Antho3004 <anthony.iv14@gmail.com>
2023-12-15 12:38:17 +0100

    add mulitburn

--- src/burn.py
+++ src/burn.py
@@ -12,41 +12,44 @@ class Burn(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def burn(self, ctx, code_card: str):
+    async def burn(self, ctx, *codes_cards: str):
         user_id = ctx.author.id
+        total_money_reward = 0  # Variable pour stocker la somme totale d'argent gagnée
 
-        # Check if the user has the card in their inventory
-        cursor.execute("SELECT * FROM user_inventaire WHERE user_id = ? AND code_card = ?", (user_id, code_card))
-        existing_card = cursor.fetchone()
+        for code_card in codes_cards:
+            # Check if the user has the card in their inventory
+            cursor.execute("SELECT * FROM user_inventaire WHERE user_id = ? AND code_card = ?", (user_id, code_card))
+            existing_card = cursor.fetchone()
 
-        if not existing_card:
-            embed = Embed(title="Card Burning Error", description=f"You do not have this card in your inventory", color=discord.Color.red())
-            await ctx.send(embed=embed)
-            return
+            if not existing_card:
+                embed = Embed(title="Card Burning Error", description=f"You do not have the card `{code_card}` in your inventory", color=discord.Color.red())
+                await ctx.send(embed=embed)
+                continue  # Move to the next card in case of error
 
-        # Get the amount of money to add (for example, 50 to illustrate, you can adjust this)
-        carte_rarete = existing_card[4]
-        if carte_rarete == "C":
-            money_reward = random.randint(10, 100)
-        elif carte_rarete == "U":
-            money_reward = random.randint(100, 200)
-        elif carte_rarete == "R":
-            money_reward = random.randint(200, 300)
-        elif carte_rarete == "E":
-            money_reward = random.randint(300, 400)
-        elif carte_rarete == "L":
-            money_reward = random.randint(400, 500)
+            # Get the amount of money to add (for example, 50 to illustrate, you can adjust this)
+            carte_rarete = existing_card[4]
+            if carte_rarete == "C":
+                money_reward = random.randint(10, 100)
+            elif carte_rarete == "U":
+                money_reward = random.randint(100, 200)
+            elif carte_rarete == "R":
+                money_reward = random.randint(200, 300)
+            elif carte_rarete == "E":
+                money_reward = random.randint(300, 400)
+            elif carte_rarete == "L":
+                money_reward = random.randint(400, 500)
 
-        # Add money to the user
-        cursor.execute("UPDATE user_data SET argent = argent + ? WHERE user_id = ?", (money_reward, user_id))
+            # Add money to the user
+            cursor.execute("UPDATE user_data SET argent = argent + ? WHERE user_id = ?", (money_reward, user_id))
+            total_money_reward += money_reward  # Ajouter la récompense à la somme totale
 
-        # Remove the card from the user's inventory
-        cursor.execute("DELETE FROM user_inventaire WHERE user_id = ? AND code_card = ?", (user_id, code_card))
+            # Remove the card from the user's inventory
+            cursor.execute("DELETE FROM user_inventaire WHERE user_id = ? AND code_card = ?", (user_id, code_card))
 
         connection.commit()
 
-        # Confirmation message in an embed
-        embed = Embed(title="Card Burned Successfully", description=f"You burned the card `{code_card}` and gained {money_reward} <:HCoins:1134169003657547847>", color=discord.Color.green())
+        # Confirmation message in an embed with the total money reward
+        embed = Embed(title="Cards Burned Successfully", description=f"You burned **{len(codes_cards)}** cards and gained a total of **{total_money_reward}** <:HCoins:1134169003657547847>", color=discord.Color.green())
         await ctx.send(embed=embed)
 
 async def setup(bot):
