85c17ee92c5949dcc66c045059582ba47ea9d9f2
Antho3004 <anthony.iv14@gmail.com>
2023-12-15 17:58:39 +0100

    update filter

--- src/inventaire.py
+++ src/inventaire.py
@@ -10,37 +10,43 @@ class Inventaire(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def show_inventory(self, ctx, user, name):
+    async def show_inventory(self, ctx, user, filter_type, filter_value):
+        # Convertir la valeur du filtre en minuscules
+        filter_value = filter_value.lower() if filter_value else None
 
-        if name:
-            cursor.execute(f"SELECT COUNT(*) FROM user_inventaire WHERE user_id = ? AND nom = ?", (str(user.id), name))
-            count = cursor.fetchone()[0]
+        if filter_type == "name":
+            # Filtrer par nom de carte
+            cursor.execute(f"SELECT COUNT(*) FROM user_inventaire WHERE user_id = ? AND LOWER(nom) = ?", (str(user.id), filter_value))
+        elif filter_type == "group":
+            # Filtrer par groupe
+            cursor.execute(f"SELECT COUNT(*) FROM user_inventaire WHERE user_id = ? AND LOWER(groupe) = ?", (str(user.id), filter_value))
+        elif filter_type == "rarity":
+            # Filtrer par groupe
+            cursor.execute(f"SELECT COUNT(*) FROM user_inventaire WHERE user_id = ? AND LOWER(rarete) = ?", (str(user.id), filter_value))
         else:
+            # Pas de filtre, afficher tous les éléments
             cursor.execute(f"SELECT COUNT(*) FROM user_inventaire WHERE user_id = ?", (str(user.id),))
-            count = cursor.fetchone()[0]
+
+        count = cursor.fetchone()[0]
 
         if count > 0:
-            if name:
-                cursor.execute(f"SELECT code_card, nom, groupe, rarete FROM user_inventaire WHERE user_id = ? AND nom = ? ORDER BY groupe, nom, rarete, CAST(SUBSTR(code_card, INSTR(code_card, '-') + 1) AS INTEGER)", (str(user.id), name))
-                print(cursor)
+            if filter_type:
+                if filter_type == "name":
+                    cursor.execute(f"SELECT code_card, nom, groupe, rarete, event FROM user_inventaire WHERE user_id = ? AND LOWER(nom) = ? ORDER BY groupe, nom, rarete, CAST(SUBSTR(code_card, INSTR(code_card, '-') + 1) AS INTEGER)", (str(user.id), filter_value))
+                elif filter_type == "group":
+                    cursor.execute(f"SELECT code_card, nom, groupe, rarete, event FROM user_inventaire WHERE user_id = ? AND LOWER(groupe) = ? ORDER BY groupe, nom, rarete, CAST(SUBSTR(code_card, INSTR(code_card, '-') + 1) AS INTEGER)", (str(user.id), filter_value))
+                elif filter_type == "rarity":
+                    cursor.execute(f"SELECT code_card, nom, groupe, rarete, event FROM user_inventaire WHERE user_id = ? AND LOWER(rarete) = ? ORDER BY groupe, nom, rarete, CAST(SUBSTR(code_card, INSTR(code_card, '-') + 1) AS INTEGER)", (str(user.id), filter_value))
             else:
-                cursor.execute(f"SELECT code_card, nom, groupe, rarete FROM user_inventaire WHERE user_id = ? ORDER BY groupe, nom, rarete, CAST(SUBSTR(code_card, INSTR(code_card, '-') + 1) AS INTEGER)", (str(user.id),))
+                cursor.execute(f"SELECT code_card, nom, groupe, rarete, event FROM user_inventaire WHERE user_id = ? ORDER BY groupe, nom, rarete, CAST(SUBSTR(code_card, INSTR(code_card, '-') + 1) AS INTEGER)", (str(user.id),))
 
             result = cursor.fetchall()
 
             if not result:
-            # Aucun résultat trouvé avec le nom spécifié
-                embed = discord.Embed(title=f"{user.name}'s inventory", description=f"No cards found in your inventory.", color=discord.Color.red())
+                # Aucun résultat trouvé avec le filtre spécifié
+                embed = discord.Embed(title=f"{user.name}'s inventory", description=f"No cards found in the inventory with the specified filter.", color=discord.Color.red())
                 await ctx.send(embed=embed)
                 return
-
-            rarity_emojis = {
-                "C": "<:C_:1107771999490686987>",
-                "U": "<:U_:1107772008193867867>",
-                "R": "<:R_:1107772004410601553>",
-                "E": "<:E_:1107772001747222550>",
-                "L": "<:L_:1107772002690945055>"
-            }
 
             chunks = [result[i:i + 9] for i in range(0, len(result), 9)]
 
@@ -50,6 +56,19 @@ class Inventaire(commands.Cog):
                 embed = discord.Embed(title=f"{user.name}'s inventory", color=discord.Color.green())
 
                 for line in chunk:
+                    if line[4] and line[4].lower() == 'xmas 2023':
+                        rarity_emojis = {
+                            "U": "<:xmas_boot:1183911398661693631>",
+                            "L": "<:xmas_hat:1183911360808112160>"
+                        }
+                    else:
+                        rarity_emojis = {
+                            "C": "<:C_:1107771999490686987>",
+                            "U": "<:U_:1107772008193867867>",
+                            "R": "<:R_:1107772004410601553>",
+                            "E": "<:E_:1107772001747222550>",
+                            "L": "<:L_:1107772002690945055>"
+                        }
                     embed.add_field(
                         name=f"{line[0]}", value=f"Name: {line[1]}\nGroup: {line[2]}\nRarity: {rarity_emojis.get(line[3], '')}\n", inline=True)
 
@@ -60,22 +79,38 @@ class Inventaire(commands.Cog):
             await paginator.start(ctx)
 
         else:
-            embed = discord.Embed(title=f"{user.name}'s inventory", description="Your inventory is empty.", color=discord.Color.red())
+            embed = discord.Embed(title=f"{user.name}'s inventory", description="The inventory is empty.", color=discord.Color.red())
             await ctx.send(embed=embed)
 
     @commands.command()
-    async def inventory(self, ctx, user: discord.Member = None, name=None):
+    async def inventory(self, ctx, user: discord.Member = None, *, filter_arg=None):
         if user is None:
             user = ctx.author
+        else:
+            # Correction pour obtenir l'utilisateur mentionné
+            user = ctx.guild.get_member(int(user.id))
 
-        await self.show_inventory(ctx, user, name)
+        if filter_arg:
+            # Séparer le type de filtre et la valeur du filtre en utilisant le signe égal (=) comme séparateur
+            filter_type, filter_value = filter_arg.split("=")
+            await self.show_inventory(ctx, user, filter_type.lower(), filter_value)
+        else:
+            await self.show_inventory(ctx, user, None, None)
 
     @commands.command(name='inv')
-    async def shortcut_inventory(self, ctx, user: discord.Member = None, name=None):
+    async def shortcut_inventory(self, ctx, user: discord.Member = None, *, filter_arg=None):
         if user is None:
             user = ctx.author
+        else:
+            # Correction pour obtenir l'utilisateur mentionné
+            user = ctx.guild.get_member(int(user.id))
 
-        await self.show_inventory(ctx, user, name)
+        if filter_arg:
+            # Séparer le type de filtre et la valeur du filtre en utilisant le signe égal (=) comme séparateur
+            filter_type, filter_value = filter_arg.split("=")
+            await self.show_inventory(ctx, user, filter_type.lower(), filter_value)
+        else:
+            await self.show_inventory(ctx, user, None, None)
 
 async def setup(bot):
     await bot.add_cog(Inventaire(bot))
