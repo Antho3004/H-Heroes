f7f21ddd5268c111f19a66527fdf67124428af77
Antho3004 <anthony.iv14@gmail.com>
2023-12-22 21:01:05 +0100

    Ajout recompense work

--- src/profil.py
+++ src/profil.py
@@ -1,6 +1,7 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import sqlite3
+
 
 connection = sqlite3.connect("HallyuHeroes.db")
 cursor = connection.cursor()
@@ -36,40 +37,37 @@ class Profil(commands.Cog):
             cursor.execute("SELECT COALESCE(legendary, 0) FROM user_data WHERE user_id = ?", (str(user.id),))
             packs_legendaire = cursor.fetchone()[0]
 
-            # Get the favorite card from the user_data table
+            cursor.execute("SELECT COALESCE(work, 0) FROM user_data WHERE user_id = ?", (str(user.id),))
+            number_work = cursor.fetchone()[0]
+
             cursor.execute("SELECT carte_favori FROM user_data WHERE user_id = ?", (str(user.id),))
             carte_favori = cursor.fetchone()[0]
 
             cursor.execute("select user_inventaire.image_url from user_data join user_inventaire on user_data.carte_favori = user_inventaire.code_card WHERE user_inventaire.user_id = ?", (str(user.id),))
             img_fav = cursor.fetchone()
 
-            # Check if the favorite card is still in the user's inventory
             cursor.execute("SELECT code_card FROM user_inventaire WHERE user_id = ? AND code_card = ?", (user.id, carte_favori))
             favorite_card_available = cursor.fetchone() is not None
 
             if not favorite_card_available:
-                # If the favorite card is not in the user's inventory, set it to None
                 carte_favori = "None"
 
-            # Check if img_fav is not None before setting the image in the embed
             if img_fav is not None:
                 img_fav = img_fav[0]
 
         else:
             embed = discord.Embed(title="**Profile Not Found**", description="You don't have an account, use `$start` to create one.", color=discord.Color.red())
             await ctx.send(embed=embed)
-            return  # Return here to avoid creating another embed for the user without a profile
+            return
 
         embed = discord.Embed(title=f"{user.name}'s profile", description=description, color=discord.Color.blue())
-        embed.add_field(name="", value=f":moneybag: **Wallet** : {formatted_argent} <:HCoins:1134169003657547847>\n:flower_playing_cards: **Inventory** : {nombre_de_cartes}\n:heart: **Favorite card** : {carte_favori}", inline=False)
-        embed.add_field(name="PACKS", value=f"<:Bronze:1136312536665440387> **Bronze** : {packs_bronze}\n<:Argent:1136312524900401213>  **Silver** : {packs_silver}\n<:Gold:1136312506957189131> **Gold** : {packs_gold}\n<:Legendary:1136312609449193544> **Legendary** : {packs_legendaire}", inline=False)
+        embed.add_field(name="", value=f":moneybag: **Wallet** : {formatted_argent} <:HCoins:1134169003657547847>\n:flower_playing_cards: **Inventory** : {nombre_de_cartes}\n:heart: **Favorite card** : {carte_favori}\n:hammer_pick: **Works** : {number_work}", inline=False)
+        embed.add_field(name="PACKS", value=f"<:Bronze:1136312536665440387> **Bronze** : {packs_bronze}\n<:Argent:1136312524900401213> **Silver** : {packs_silver}\n<:Gold:1136312506957189131> **Gold** : {packs_gold}\n<:Legendary:1136312609449193544> **Legendary** : {packs_legendaire}", inline=False)
         embed.add_field(name="ACHIEVEMENT", value=f"Soon\n", inline=False)
 
-        # Set the image in the embed
         embed.set_image(url=img_fav)
 
         await ctx.send(embed=embed)
-
 
     @commands.command()
     async def profile(self, ctx, user: discord.Member = None):
@@ -99,7 +97,6 @@ class Profil(commands.Cog):
 
         connection.commit()
 
-        # Create an embed for the updated description response
         updated_embed = discord.Embed(title="**UPDATED DESCRIPTION**", description=new_description, color=discord.Color.green())
         await ctx.send(embed=updated_embed)
 
--- src/work.py
+++ src/work.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import sqlite3
 import random
 import datetime
@@ -18,23 +19,65 @@ def has_favorite_card():
             return True
         else:
             embed = discord.Embed(title=f"{user.name} - **WORK**", description="You don't have a favorite card. Please choose one using the command `$fav or $favorite`", color=discord.Color.red())
-            await ctx.send(embed=embed)  # Await the send() function here
+            await ctx.send(embed=embed)
             return False
 
     return commands.check(predicate)
 
+# Fonction pour formater l'argent
+def format_money(money):
+    return "{:,}".format(money).replace(",", " ")
+
+# Fonction de récompense hebdomadaire
+async def weekly_reward():
+    # Récupérer tous les utilisateurs
+    cursor.execute("SELECT user_id, work FROM user_data")
+    users = cursor.fetchall()
+
+    # Parcourir tous les utilisateurs
+    for user_id, work_count in users:
+        reward = 0
+        if work_count >= 300:
+            reward = 100000
+        elif 250 <= work_count <= 299:
+            reward = 70000
+        elif 200 <= work_count <= 249:
+            reward = 50000
+        elif 150 <= work_count <= 199:
+            reward = 30000
+        elif 100 <= work_count <= 149:
+            reward = 20000
+        elif 50 <= work_count <= 99:
+            reward = 10000
+        elif 10 <= work_count <= 49:
+            reward = 5000
+
+        # Mettre à jour l'argent de l'utilisateur
+        cursor.execute("UPDATE user_data SET argent = argent + ? WHERE user_id = ?", (reward, user_id))
+
+    # Réinitialiser le compteur de travail pour tous les utilisateurs
+    cursor.execute("UPDATE user_data SET work = 0")
+    connection.commit()
+
 class Work(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    def format_money(self, money):
-        return "{:,}".format(money).replace(",", " ")
+        self.scheduler = AsyncIOScheduler()
+        self.scheduler.add_job(weekly_reward, trigger="cron", day_of_week="fri", hour=18, minute=00, second=0)
+        self.scheduler.start()
+
+    def cog_unload(self):
+        self.scheduler.shutdown()
 
     @commands.command()
-    @has_favorite_card()  # Apply the custom check decorator
+    @has_favorite_card()
     @commands.cooldown(1, 1800, commands.BucketType.user)
     async def work(self, ctx):
         user = ctx.author
+
+        # Increment the 'work' column in the 'user_data' table
+        cursor.execute("UPDATE user_data SET work = work + 1 WHERE user_id = ?", (str(user.id),))
+        connection.commit()
 
         cursor.execute("SELECT i.rarete FROM user_data AS u JOIN user_inventaire AS i ON u.carte_favori = i.code_card WHERE u.user_id = ?", (str(user.id),))
         result = cursor.fetchone()
@@ -58,9 +101,9 @@ class Work(commands.Cog):
         # Fetch the updated amount after the update
         cursor.execute("SELECT argent FROM user_data WHERE user_id = ?", (str(user.id),))
         updated_amount = cursor.fetchone()[0]
-        uptated_formatted_amount = self.format_money(updated_amount)
+        updated_formatted_amount = format_money(updated_amount)
 
-        embed = discord.Embed(title=f"{user.name} - **WORK**", description=f"You have earned **{montant}** <:HCoins:1134169003657547847> from your last job!\n\nNew balance : **{uptated_formatted_amount}** <:HCoins:1134169003657547847>", color=discord.Color.green())
+        embed = discord.Embed(title=f"{user.name} - **WORK**", description=f"You have earned **{montant}** <:HCoins:1134169003657547847> from your last job!\n\nNew balance : **{updated_formatted_amount}** <:HCoins:1134169003657547847>", color=discord.Color.green())
         await ctx.send(embed=embed)
 
 async def setup(bot):
