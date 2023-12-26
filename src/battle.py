548d11d5cebdc952c78997799d4cbef2b4f77a82
Antho3004 <anthony.iv14@gmail.com>
2023-12-26 20:42:09 +0100

    Ajout de la commande rank_stats

---
+++ src/battle.py
@@ -0,0 +1,71 @@
+import discord
+from discord.ext import commands
+import sqlite3
+from dispie import Paginator
+
+connection = sqlite3.connect("HallyuHeroes.db")
+cursor = connection.cursor()
+
+class Battle(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def rank_stats(self, ctx, member: discord.Member = None):
+        user_id = member.id if member else ctx.author.id
+        user_name = member.name if member else ctx.author.name
+
+        # Récupérer les cartes de l'utilisateur depuis la base de données
+        cursor.execute("SELECT nom, groupe, code_card, rarete, event, chant, dance, rap, acting, modeling FROM user_inventaire WHERE user_id = ?", (user_id,))
+        user_cards = cursor.fetchall()
+
+        # Calculer la somme des compétences pour chaque carte et les classer
+        ranked_cards = sorted(user_cards, key=lambda card: sum(card[5:]), reverse=True)
+
+        # Créer des pages pour l'affichage paginé
+        chunks = [ranked_cards[i:i + 10] for i in range(0, len(ranked_cards), 10)]
+
+        embeds = []
+        for i, chunk in enumerate(chunks):
+            embed = discord.Embed(title=f"Rank Stat - {user_name}", color=discord.Color.blue())
+            for j, card in enumerate(chunk):
+                position = i * 10 + j + 1
+                total_score = sum(card[5:])
+                if position == 1:
+                    emoji = ":first_place:"
+                elif position == 2:
+                    emoji = ":second_place:"
+                elif position == 3:
+                    emoji = ":third_place:"
+                else:
+                    emoji = f"{position}."
+                
+                if card[4] and card[4].lower() == 'xmas 2023':
+                    rarity_emojis = {
+                        "U": "<:xmas_boot:1183911398661693631>",
+                        "L": "<:xmas_hat:1183911360808112160>"
+                    }
+                elif card[4] and card[4].lower() == 'new year 2024':
+                    rarity_emojis = {
+                        "R": "<:NY_Confetti:1185996235551805470>",
+                        "L": "<:NY_Fireworks:1185996232477384808>"
+                    }
+                else:
+                    rarity_emojis = {
+                        "C": "<:C_:1107771999490686987>",
+                        "U": "<:U_:1107772008193867867>",
+                        "R": "<:R_:1107772004410601553>",
+                        "E": "<:E_:1107772001747222550>",
+                        "L": "<:L_:1107772002690945055>"
+                    }
+                embed.add_field(name=f"{emoji} {card[0]} {card[1]} {rarity_emojis.get(card[3], '')}", value=f"Code: `{card[2]}` : **{total_score}**", inline=False)
+
+            embed.set_footer(text=f"Page {i + 1}/{len(chunks)}")
+            embeds.append(embed)
+
+        # Afficher les pages avec Paginator
+        paginator = Paginator(embeds)
+        await paginator.start(ctx)
+
+async def setup(bot):
+    await bot.add_cog(Battle(bot))
